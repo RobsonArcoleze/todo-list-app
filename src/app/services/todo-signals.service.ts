269a12da56a0f6fc15bb8b471fa442a79f6f7997
RobsonArcoleze <robson.arcoleze@artesanalinvestimetos.com.br>
2023-08-11 21:58:07 -0300

    Feat: Implementação de serviço

---
+++ src/app/services/todo-signals.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, signal } from '@angular/core';
+import { Todo } from '../models/model/todo.model';
+import { TodoKeyLocalStorage } from '../models/enum/todoKeyLocalStorage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoSignalsService {
+  public todosState = signal<Todo[]>([]);
+
+  public updateTodos({id, title, description, done}: Todo): void{
+    if((title && id && description !== null) || undefined){
+      this.todosState.mutate((todo) => {
+        if(todo !== null){
+          todo.push(new Todo(id, title, description, done))
+        }
+      });
+      this.saveTodosInLocalStorage();
+    }
+  }
+
+  public saveTodosInLocalStorage(): void{
+    const todos = JSON.stringify(this.todosState());
+    todos && localStorage.setItem(TodoKeyLocalStorage.TODO_LIST, todos);
+  }
+}
